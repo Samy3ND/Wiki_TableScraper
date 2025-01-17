227752230213e5ea30bbd772563d4dbdd51f5829
Samyog KC <157798225+Samy3ND@users.noreply.github.com>
2025-01-17 09:03:02 +0545

    wiki.py

--- wiki.py
+++ wiki.py
@@ -48,4 +48,4 @@ if st.button("SCRAPE"):
     else:
         st.warning("Please enter a valid Wikipedia URL starting with 'https://en.wikipedia.org'.")
         
-st.markdown("Developed by : Samy3ND")
+st.markdown("Developed by : Samyog :smile") 
